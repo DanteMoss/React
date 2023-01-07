b7602d87602e3996783ab0ef8b044207c1b46439
dantemoss <dantemoss6@gmail.com>
2023-01-07 03:08:39 -0300

    cambio completo de navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,28 +3,23 @@ import './style.css';
 import CartWidget from '../CartWidget'
 import { Link } from "react-router-dom";
 
+
 export default function NavBar() {
 
     return (
-        <ul className="nav-list navbar-dark bg-dark">
-            <li className="nav-item">
-                <Link to="/">FreshNFT</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/category/Kanpai Pandas">Kanpai Pandas Collection</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/category/CryptoPunk">CryptoPunk Collection</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/category/Lil Pudgy">Lil Pudgy</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/category/3Landers">3Landers</Link>
-            </li>
+    <div>
+        <nav>
+            <h2 className="logo"><Link to="/">FreshNFT</Link></h2>
+            <ul>
+                <li><Link to="/category/Kanpai Pandas">Kanpai Pandas Collection</Link></li>
+                <li><Link to="/category/CryptoPunk">CryptoPunk Collection</Link></li>
+                <li><Link to="/category/Lil Pudgy">Lil Pudgy</Link></li>
+                <li><Link to="/category/3Landers">3Landers</Link></li>
+            </ul>
             <div className="widget-container">
-                <CartWidget/>
+            <CartWidget/>
             </div>
-        </ul>
+        </nav>
+    </div>
     );
 }
